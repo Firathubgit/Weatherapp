af42841c281a19dfd617cf9971fc318c88f79b4f
Firathubgit <Firat05_@hotmail.com>
2025-02-02 04:01:25 +0100

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f8a7ff2f8emsh8ea5773c07cb6ecp1c0e92jsna95f4698b4ed',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "d4598c2ef4f429035e5afdcd8bf8ea0f";
+
